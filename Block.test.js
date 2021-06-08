e3943ef6a1d8bf8ca9748b0bf50e54c73ed456af
jestemAria <radmehr.krash@gmail.com>
2021-06-09 00:38:17 +0430

    The new mined blocks now have a hash, based on `timestamp`, `previousBlockHash` and `data`. Testing minable blocks passed successfully.

--- Block.test.js
+++ Block.test.js
@@ -1,5 +1,6 @@
 const Block = require('./Block');
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 describe('Block', () => {
     const timestamp = 'a-date'; 
@@ -48,5 +49,9 @@ describe('Block', () => {
         it('sets a `timestamp`', () => {
             expect(minedBlock.timestamp).not.toEqual(undefined);
         });
+
+        it('creates a SHA-256 `hash` based on the proper inputs', () => {
+            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+        })
     });
 });
