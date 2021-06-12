27a40ef4a42d79e9d912fba8909306b86c8f788a
jestemAria <radmehr.krash@gmail.com>
2021-06-12 22:13:11 +0430

    Test cases created for validating a blockchain and need to prepared with expect statements in blockchain.test.js .

--- blockchain.test.js
+++ blockchain.test.js
@@ -18,4 +18,24 @@ describe('Blockchain', () => {
 
         expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
     });
+
+    describe('isValidChain()', () => {
+        describe('starts with genesis block and has multiple blocks', () => {
+            describe('and a lastHash refrence has changed', () => {
+                it('returns false', () => {});
+            });
+
+            describe('the chain contains a block with an invalid field', () => {
+                it('returns false', () => {});
+            });
+
+            describe('the chain deos not contain any invalid blocks', () => {
+                it('returns true', () => {});
+            });
+        });
+
+        describe('chain does not starts with genesis block', () => {
+            it('returns false', () => {});
+        });
+    });
 });
