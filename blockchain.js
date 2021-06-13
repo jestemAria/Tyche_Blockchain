d1f843ed935701974a6d78dd37e7c658ecf94485
jestemAria <radmehr.krash@gmail.com>
2021-06-13 23:41:46 +0430

    blockchain.js now updated and tests passed about validating blochchain. Now we are sure the blockchain is a real chain with same last hash in new block and the block hash is exactly the validated hash.

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain{
     constructor(){
@@ -12,6 +13,32 @@ class Blockchain{
         });
         
         this.chain.push(newBlock);
+    }
+
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) != JSON.stringify(Block.genesis())){
+            return false
+        };
+
+        for (let counter = 1 ; counter < chain.length ; counter++){
+            const block = chain[counter];
+
+            const actualLastHash = chain[counter-1].hash;
+
+            const { timestamp, lastHash, hash, data } = block;
+
+            if (lastHash != actualLastHash){
+                return false
+            };
+            
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if (hash != validatedHash){
+                return false
+            };
+        }
+
+        return true;
     }
 }
 
