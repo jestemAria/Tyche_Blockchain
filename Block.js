0b4ea6b60b00806b539a2444e4f6997ce2b5e546
jestemAria <radmehr.krash@gmail.com>
2022-02-15 20:02:33 +0330

    adjustDifficulty method added to make difficulty dynamic. It sets the new difficulty based on calculating timestamp.

--- Block.js
+++ Block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 class Block{
     constructor({ timestamp, lastHash, hash, data, nonce, difficulty }){
@@ -23,11 +23,21 @@ class Block{
         do {
             nonce ++;
             timestamp = Date.now();
-            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
         }
         while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
+    }
+
+    static adjustDifficulty({ originalBlock, timestamp }) {
+        const { difficulty } = originalBlock;
+
+        const difference = timestamp - originalBlock.timestamp;
+        
+        if (difference > MINE_RATE ) return difficulty -1;
+        
+        return difficulty + 1;
     }
 }
 
