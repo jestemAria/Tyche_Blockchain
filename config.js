012a2b2bc8be4bc0df473e9241f3e6a4334a45f4
jestemAria <radmehr.krash@gmail.com>
2022-02-15 19:20:42 +0330

    INITIAL_DIFFICULTY added to the config.js also difficulty and nonce added as parameter to the GENESIS_DATA

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '----',
     hash: '_hash_',
-    data: []
+    data: [],
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0
 };
 
 module.exports = { GENESIS_DATA };
