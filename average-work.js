4995d0e3d5f01375285cc06f0e37fef79260a3e2
jestemAria <radmehr.krash@gmail.com>
2022-02-18 00:58:38 +0330

    we print the first block to monitor the hash not to be in the binary form also logging the block mining is now arranged while printing

--- average-work.js
+++ average-work.js
@@ -4,6 +4,8 @@ const blockchain = new Blockchain();
 
 blockchain.addBlock({ data: 'initial' });
 
+console.log('First block:', blockchain.chain[blockchain.chain.length - 1]);
+
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
@@ -20,5 +22,5 @@ for (let index=0; index<10000; index++){
 
     average = times.reduce((total, next) => total + next) / times.length;
 
-    console.log(`Time to mine block ${index}: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty} Average time: ${average}ms`);
+    console.log(`Time to mine block ${index}: ${timeDiff}ms\t Difficulty: ${nextBlock.difficulty}\t Average time: ${average}ms`);
 }
