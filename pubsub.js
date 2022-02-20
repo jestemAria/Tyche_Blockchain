60650c56f337767e4c5877674231e6731821f059
jestemAria <radmehr.krash@gmail.com>
2022-02-20 16:21:14 +0330

    pubsub now added as an instance for index.js

--- pubsub.js
+++ pubsub.js
@@ -1,15 +1,18 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST: 'TEST'
+    TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
 };
 
 class PubSub {
-    constructor() {
+    constructor({ blockchain }) {
+        this.blockchain = blockchain;
+
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
 
-        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscribeToChannels();
 
         this.subscriber.on(
             'message',
@@ -18,10 +21,31 @@ class PubSub {
     }
 
     handleMassage(channel, message) {
-        console.log(`Message received. Channel: ${channel}. Message: ${message}`)
+        console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+        const parsedMessage = JSON.parse(message);
+
+        if (channel === CHANNELS.BLOCKCHAIN) {
+            this.blockchain.replaceChain(parsedMessage);
+        }
+    }
+
+    subscribeToChannels() {
+        Object.values(CHANNELS).forEach(channel => {
+            this.subscriber.subscribe(channel);
+        });
+    }
+
+    publish({ channel, message }) {
+        this.publisher.publish(channel, message);
+    }
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
     }
 }
 
-const testPubSub = new PubSub();
-
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
