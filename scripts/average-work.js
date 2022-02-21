d220171ac590951599e67adcc7a1ad03c680aa33
jestemAria <radmehr.krash@gmail.com>
2022-02-21 23:15:31 +0330

    average-works now added to the scripts directory

---
+++ scripts/average-work.js
@@ -0,0 +1,26 @@
+const Blockchain = require('../blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initial' });
+
+console.log('First block:', blockchain.chain[blockchain.chain.length - 1]);
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let index=0; index<10000; index++){
+    prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
+
+    blockchain.addBlock({ data: `block ${index}` });
+    nextBlock = blockchain.chain[blockchain.chain.length-1];
+
+    nextTimestamp = nextBlock.timestamp;
+    timeDiff = nextTimestamp - prevTimestamp;
+    times.push(timeDiff);
+
+    average = times.reduce((total, next) => total + next) / times.length;
+
+    console.log(`Time to mine block ${index + 2}: ${timeDiff}ms\t Difficulty: ${nextBlock.difficulty}\t Average time: ${average}ms`);
+}
