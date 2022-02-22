0cefa49b39ba8523c8293af7c27b7999b55a1c90
jestemAria <radmehr.krash@gmail.com>
2022-02-22 18:00:23 +0330

    tests now describe a signing data whether it verifies a signature or not

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
     let wallet;
@@ -15,5 +16,27 @@ describe('Wallet', () => {
         expect(wallet).toHaveProperty('publicKey');
     });
 
+    describe('signing data', () => {
+        const data = 'dummy-data';
 
+        it('verifies a signature', () => {
+            expect(    
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data),
+                })
+            ).toBe(true);
+        });
+
+        it('does not verify an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false);
+        });
+    });
 });
