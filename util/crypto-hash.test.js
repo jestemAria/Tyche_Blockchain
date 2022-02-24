fe1d6502e5ea0912245a6407a103196bbe570ad4
jestemAria <radmehr.krash@gmail.com>
2022-02-24 23:42:16 +0330

    from now on  produces a unique hash when the properties have changed on an input

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,10 +2,19 @@ const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', () => {
     it('generates a SHA-256 hashed output', () => {
-        expect(cryptoHash('dummy')).toEqual('b5a2c96250612366ea272ffac6d9744aaf4b45aacd96aa7cfcb931ee3b558259');
+        expect(cryptoHash('dummy'))
+            .toEqual('ebbe2dfbb019b8256cb73d309d38f8478b7a3aec5aef544f1aa69087aedb713b');
     });
 
     it('produces the same hash with the same input arguments in any kind of order.', () => {
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
+    });
+
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const dummy = {};
+        const originalHash = cryptoHash(dummy);
+        dummy['a'] = 'a';
+
+        expect(cryptoHash(dummy)).not.toEqual(originalHash);
     });
 });
