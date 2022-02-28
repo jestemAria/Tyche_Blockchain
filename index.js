9a3bd09e642d1874bce65ebb3a66d8d9b010a514
jestemAria <radmehr.krash@gmail.com>
2022-02-28 22:17:35 +0330

    broadcastTransaction now will be used to get all transactions once a new peer joins the network

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain;
 const transactionPool = new TransactionPool;
 const wallet = new Wallet;
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -47,6 +47,8 @@ app.post('/api/transact', (req, res) => {
     }
 
     transactionPool.setTransaction(transaction);
+
+    pubsub.broadcastTransaction(transaction);
 
     res.json({ type: 'success', transaction });
 });
