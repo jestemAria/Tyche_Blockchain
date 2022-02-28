f431e104f51b25e279bbbb6156b207011c7f2c24
jestemAria <radmehr.krash@gmail.com>
2022-02-28 22:02:01 +0330

    a new get request for transaction-pool-map now added as an api

--- index.js
+++ index.js
@@ -48,9 +48,11 @@ app.post('/api/transact', (req, res) => {
 
     transactionPool.setTransaction(transaction);
 
-    console.log('transactionPool', transactionPool);
-
     res.json({ type: 'success', transaction });
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
