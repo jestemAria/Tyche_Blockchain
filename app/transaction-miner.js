6bdacaaab377260897d1b6459af8b054fba0c858
jestemAria <radmehr.krash@gmail.com>
2022-03-02 19:44:30 +0330

    TransactionMiner class now developed for mine transactions

---
+++ app/transaction-miner.js
@@ -0,0 +1,14 @@
+class TransactionMiner {
+    constructor({ blockchain, transactionPool, wallet, pubsub }) {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.pubsub = pubsub;
+    }
+
+    mineTransactions() {
+
+    }
+}
+
+module.exports = TransactionMiner;
