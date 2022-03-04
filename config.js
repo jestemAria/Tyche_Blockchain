a28bc3e6cbf3d35593b1f7edb63813f15bf74506
jestemAria <radmehr.krash@gmail.com>
2022-03-04 22:23:59 +0330

    new `MINING_INPUT` and `MINING_REWARD` now added

--- config.js
+++ config.js
@@ -12,4 +12,16 @@ const GENESIS_DATA = {
 
 const STARTING_BALANCE = 8;
 
-module.exports = { GENESIS_DATA , MINE_RATE , STARTING_BALANCE };
+const REWARD_INPUT = {
+    address: '*authorized-reward*'
+};
+
+const MINING_REWARD = 16;
+
+module.exports = {
+    GENESIS_DATA,
+    MINE_RATE,
+    STARTING_BALANCE,
+    REWARD_INPUT,
+    MINING_REWARD
+};
