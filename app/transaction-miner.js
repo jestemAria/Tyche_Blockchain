b11e5af4ab3d36d0605078bec2defd4e9bf8a9d7
jestemAria <radmehr.krash@gmail.com>
2022-03-05 16:54:17 +0330

    mineTransactions now can get transaction-pool's valid transactions, add it to the blockchain, broadcast the chain and finally clear the transaction-pool

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
     constructor({ blockchain, transactionPool, wallet, pubsub }) {
         this.blockchain = blockchain;
@@ -7,7 +9,15 @@ class TransactionMiner {
     }
 
     mineTransactions() {
+        const validTransactions = this.transactionPool.validTransactions();
 
+        validTransactions.push(Transaction.rewardTransaction({ minerWallet: this.wallet }));
+
+        this.blockchain.addBlock({ data: validTransactions });
+
+        this.pubsub.broadcastChain();
+
+        this.transactionPool.clear();
     }
 }
 
