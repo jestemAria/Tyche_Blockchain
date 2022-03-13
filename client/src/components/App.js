a0211fcfeca86d9e2d149618ae3ee8f1e022ad5d
jestemAria <radmehr.krash@gmail.com>
2022-03-13 23:55:56 +0330

    from now on, states added to the project and fetching data from the back-end to the front-end was successful

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component{
+    state = { walletInfo: {} };
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info')
+            .then(response => response.json())
+                .then(json => this.setState({ walletInfo: json }));
+    }
+
     render() {
+        const { address, balance } = this.state.walletInfo;
+
         return (
             <div>
+                <div>
                 Welcome to the Blockchain!
+                </div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
             </div>
-        )
+        );
     }
 }
 
