65a2f0066c782ec9d875bf907a6562df77bbd114
jestemAria <radmehr.krash@gmail.com>
2022-03-13 18:07:30 +0330

    index.js now linked to the App.js from the components directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Tyche Blockchain:Here is Live Update</div>,
+    <App />,
     document.getElementById('root')
 );
