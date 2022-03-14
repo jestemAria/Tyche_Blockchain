31d9c9e9b795d25ed6d05ab1000b9408a76fd495
jestemAria <radmehr.krash@gmail.com>
2022-03-14 17:33:25 +0330

    from now on, Blocks component is reachable from the App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
 
 class App extends Component{
     state = { walletInfo: {} };
@@ -19,6 +20,8 @@ class App extends Component{
                 </div>
                 <div>Address: {address}</div>
                 <div>Balance: {balance}</div>
+                <br />
+                <Blocks />
             </div>
         );
     }
